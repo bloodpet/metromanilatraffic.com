78aa631c15b3e66c3367ecd92f0d064372f345c7
Eman Calso <egcalso@gmail.com>
2011-06-03 13:02:38 +0800

    Fix create_sections backend.

--- apps/core/backend.py
+++ apps/core/backend.py
@@ -11,6 +11,7 @@ def generate_sections(road, direction):
     for count, node1, node2 in zip(range(node_count), nodes[:node_count], nodes[1:]):
         section1 = road.section_set.create(name='%s to %s' % (node1.name, node2.name), start=node1, end=node2, direction=dir1, position=count)
         section1.save()
-        section2 = road.section_set.create(name='%s to %s' % (node2.name, node1.name), start=node2, end=node1, direction=dir2, position=count)
+        # Fix the position of the northbound/eastbound sections to allow viewing them from top to bottom
+        section2 = road.section_set.create(name='%s to %s' % (node2.name, node1.name), start=node2, end=node1, direction=dir2, position=(node_count+1-count))
         section2.save()
     return ''
