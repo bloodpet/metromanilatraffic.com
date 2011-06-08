590feada167615c0aaee974c55c07a8b68c5ecb1
Eman Calso <egcalso@gmail.com>
2011-06-08 16:34:46 +0800

    Add change password page.

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
-from django.contrib.auth.views import password_reset,password_reset_done,password_reset_confirm
+from django.contrib.auth.views import password_reset, password_reset_done, password_reset_confirm, password_change, password_change_done
 import views as toplevel
 #import fbconnect, twitter
 
@@ -13,6 +13,8 @@ urlpatterns = patterns(
     #url(r'^twitter/callback/$', twitter.process_callback, name='accounts-twitter-callback'),
     url(r'^logout/$', toplevel.logout, name='accounts-logout'),
     url(r'^login/$', toplevel.login, name='accounts-login'),
+    url(r'^password/change/done/$', password_change_done, {'template_name': 'accounts/password_change_done.html'},name="password-change-done"),
+    url(r'^password/change/$', password_change,{'template_name': 'accounts/password_change.html'},name="password-change"),
     url(r'^password/reset/done/$', password_reset_done, {'template_name': 'accounts/password_reset_done.html'},name="password-reset-done"),
     url(r'^password/reset/$', password_reset,{'template_name': 'accounts/password_reset.html','email_template_name':'accounts/password_reset_email.html'},name="password-reset"),
     url(r'^password/reset/confirm/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)$',password_reset_confirm,{'template_name':'accounts/password_reset_confirm.html'},name="password-reset-confirm"),
