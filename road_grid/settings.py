633c8f44e0060d10f3fa8edf825cca61b5fb73d0
Eman Calso <egcalso@gmail.com>
2011-07-19 09:22:08 +0800

    Make use of cache middleware.

--- road_grid/settings.py
+++ road_grid/settings.py
@@ -98,11 +98,13 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.cache.FetchFromCacheMiddleware',
 )
 
 ROOT_URLCONF = 'road_grid.urls'
