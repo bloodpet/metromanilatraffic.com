0204a479ab7e4d1b9f9ec69ffc5933b6ece03bd5
Eman Calso <egcalso@gmail.com>
2011-08-07 22:36:28 +0800

    Update latest_rating on sections.

--- apps/tv5/parser.py
+++ apps/tv5/parser.py
@@ -156,6 +156,9 @@ class RoadParser(object):
     def post_situations(self):
         situations = []
         for section, rate, time in self.section_status.get_rates():
+            saction.latest_rating = rate
+            saction.status_at = time
+            saction.save()
             situation = Situation(
                 section = section,
                 rating = rate,
